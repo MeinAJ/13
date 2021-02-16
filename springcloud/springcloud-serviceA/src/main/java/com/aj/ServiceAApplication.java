168f17f36d2fbead4f0b047033b9ba2670effcb4
unknown <15102356906@163.com>
2021-02-16 16:46:51 +0800

    second version

--- springcloud/springcloud-serviceA/src/main/java/com/aj/ServiceAApplication.java
+++ springcloud/springcloud-serviceA/src/main/java/com/aj/ServiceAApplication.java
@@ -2,14 +2,14 @@ package com.aj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
-public class EurekaserverApplication {
+public class ServiceAApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaserverApplication.class, args);
+        SpringApplication.run(ServiceAApplication.class, args);
     }
 
 }
