75a7803191057e2409fed28591b780364e44df83
meinaj_foundao <anjun@foundao.com>
2021-02-24 18:32:59 +0800

    eureka-server修改成单机运行

---
+++ springcloud/client/src/main/java/com/aj/controller/GreetingController.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019, ABB and/or its affiliates. All rights reserved.
+ * ABB PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.aj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return "hello, " + name;
+    }
+
+}
