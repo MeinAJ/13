7dc29d9b3b26bcc9dbc73066f6dd7caa17911acd
meinaj_foundao <anjun@foundao.com>
2021-03-01 16:21:04 +0800

    更新

--- springcloud/netflix-ribbon/src/main/java/com/aj/GreetingClient.java
+++ springcloud/netflix-ribbon/src/main/java/com/aj/GreetingClient.java
@@ -1,10 +1,6 @@
 package com.aj;
 
 
-import com.netflix.client.http.HttpRequest;
-import com.netflix.client.http.HttpResponse;
-import com.netflix.niws.client.http.RestClient;
-
 /**
  * GreetingClient
  *
@@ -14,25 +10,36 @@ import com.netflix.niws.client.http.RestClient;
 public class GreetingClient {
 
     public static void main(String[] args) throws Exception {
-        // 基于ribbon的api来开发一个可以负载均衡调用一个服务的代码
+//        // 基于ribbon的api来开发一个可以负载均衡调用一个服务的代码
+//
+//        // 首先使用代码的方式对ribbon进行一下配置，配置一下ribbon要调用的那个服务的server list
+//        ConfigurationManager.getConfigInstance().setProperty(
+//                "greeting-service.ribbon.listOfServers", "localhost:8080,localhost:8088");
+//
+//        // 获取指定服务的RestClient，用于请求某个服务的client
+//        RestClient restClient = (RestClient) ClientFactory.getNamedClient("greeting-service");
+//
+//        // 你要请求哪个接口，构造一个对应的HttpRequest
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri("/greeting/sayHello/leo")
+//                .build();
+//
+//        // 模拟请求一个接口10次
+//        for (int i = 0; i < 10; i++) {
+//            HttpResponse response = restClient.executeWithLoadBalancer(request);
+//            String result = response.getEntity(String.class);
+//            System.out.println(result);
+//        }
+        ILoadBalancer balancer = new BaseLoadBalancer();
 
-        // 首先使用代码的方式对ribbon进行一下配置，配置一下ribbon要调用的那个服务的server list
-        ConfigurationManager.getConfigInstance().setProperty(
-                "greeting-service.ribbon.listOfServers", "localhost:8080,localhost:8088");
+        List<Server> servers = new ArrayList<Server>();
+        servers.add(new Server("localhost", 8080));
+        servers.add(new Server("localhost", 8088));
+        balancer.addServers(servers);
 
-        // 获取指定服务的RestClient，用于请求某个服务的client
-        RestClient restClient = (RestClient) ClientFactory.getNamedClient("greeting-service");
-
-        // 你要请求哪个接口，构造一个对应的HttpRequest
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri("/greeting/sayHello/leo")
-                .build();
-
-        // 模拟请求一个接口10次
         for (int i = 0; i < 10; i++) {
-            HttpResponse response = restClient.executeWithLoadBalancer(request);
-            String result = response.getEntity(String.class);
-            System.out.println(result);
+            Server server = balancer.chooseServer(null);
+            System.out.println(server);
         }
     }
 
