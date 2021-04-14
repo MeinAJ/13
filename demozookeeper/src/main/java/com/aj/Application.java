724db4ccde32f3bca5fca672880c6adf368ae4f6
unknown <15102356906@163.com>
2021-04-14 21:36:11 +0800

    update zookeeper

--- demozookeeper/src/main/java/com/aj/Application.java
+++ demozookeeper/src/main/java/com/aj/Application.java
@@ -8,6 +8,7 @@ package com.aj;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 /**
@@ -21,15 +22,21 @@ public class Application {
     public static void main(String[] args) throws Exception {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
         CuratorFramework client = CuratorFrameworkFactory.newClient(
-                "192.168.31.184:2181,192.168.31.207:2181,192.168.31.192:2181",
+                "192.168.0.207:2181,192.168.0.208:2181,192.168.0.209:2181",
                 retryPolicy);
         client.start();
 
-        client.create()
-                .creatingParentsIfNeeded()
-                .forPath("/user/dir", "hello world".getBytes());
+        String path = "/my/path" + System.currentTimeMillis();
+        client.create().creatingParentsIfNeeded().forPath(path, "hello world".getBytes());
 
-        System.out.println(new String(client.getData().forPath("/my/path")));
+        System.out.println(new String(client.getData().forPath(path)));
+
+        InterProcessMutex lock = new InterProcessMutex(client, "/locks/lock_01" + System.currentTimeMillis());
+        lock.acquire();
+
+        System.out.println("do something");
+
+        lock.release();
     }
 
 }
