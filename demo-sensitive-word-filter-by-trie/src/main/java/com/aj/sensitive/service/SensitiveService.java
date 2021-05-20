a27fd1d1139c0376a376e10a6134f0a426be06c0
unknown <15102356906@163.com>
2021-05-20 21:56:28 +0800

    update

--- demo-sensitive-word-filter-by-trie/src/main/java/com/aj/sensitive/service/SensitiveService.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019, ABB and/or its affiliates. All rights reserved.
- * ABB PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-package com.aj.sensitive.service;
-
-public interface SensitiveService {
-
-}
