1cf4eb0c5ce6955cb68fe96bf8a103684dd1b078
unknown <15102356906@163.com>
2021-05-21 00:53:24 +0800

    dd

--- demo-main/src/main/java/com/aj/ObjectMain1.java
+++ demo-main/src/main/java/com/aj/ObjectMain1.java
@@ -1,19 +1,20 @@
 package com.aj;
 
-import java.io.*;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.Map;
 
-public class ObjectMain {
+public class ObjectMain1 {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         File file = new File("D:\\1.sql");
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        HashSet hashSet = new HashSet();
+        TrieTree dic = TrieTree.getInstance;
         String line;
         while ((line = reader.readLine()) != null){
-            hashSet.add(line);
+            dic.insert(line);
         }
 
         while (true){
