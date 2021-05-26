5c450c51a133c3bcb8126c1aafa24009b82f5061
meinaj_foundao <anjun@foundao.com>
2021-05-26 22:31:29 +0800

    dd

---
+++ demo-algorithm/src/main/java/com/aj/algorithm/LeetcodeOffer07.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019, ABB and/or its affiliates. All rights reserved.
+ * ABB PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.aj.algorithm;
+
+/**
+ * https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof/
+ *
+ * @author An Jun
+ * @date 2021-05-26
+ */
+public class LeetcodeOffer07 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        return null;
+    }
+
+    public static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+    }
+
+}
