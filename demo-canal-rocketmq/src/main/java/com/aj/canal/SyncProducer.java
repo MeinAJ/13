76b0d4f096b3f77c3a73d3c02a03cf3489c844d8
unknown <15102356906@163.com>
2021-06-01 00:03:42 +0800

    update

---
+++ demo-canal-rocketmq/src/main/java/com/aj/canal/SyncProducer.java
@@ -0,0 +1,30 @@
+package com.aj.canal;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class SyncProducer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("test-002");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("192.168.0.203:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("test-002" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
