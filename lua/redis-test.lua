90180cb20fe73eef303c80f11c4aea74705e8278
meinaj_foundao <anjun@foundao.com>
2021-06-08 15:41:19 +0800

    update

--- lua/redis-test.lua
+++ lua/redis-test.lua
@@ -1,3 +1,4 @@
+---------------------------------------------------------------------------------------------
 local redis = require 'redis'
 local host = "192.168.2.52"
 local port = 6379
@@ -6,10 +7,8 @@ client = redis.connect(host, port)
 redis.call = function(cmd, ...) 
     return assert(loadstring('return client:'.. string.lower(cmd) ..'(...)'))(...)
 end
-
--- 备注
+---------------------------------------------------------------------------------------------
 
 redis.call("set","key001","123")
 local value = redis.call("get","key001")
 print(value)
-
