cc2a7a690e89bf442cc55e14513177c4b874df33
meinaj_foundao <anjun@foundao.com>
2021-06-08 16:38:24 +0800

    update

--- lua/redis-bucket-limit.lua
+++ lua/redis-bucket-limit.lua
@@ -7,14 +7,23 @@ client = redis.connect(host, port)
 redis.call = function(cmd, ...) 
     return assert(loadstring('return client:'.. string.lower(cmd) ..'(...)'))(...)
 end
----------------------------------------------------------------------------------------------
 
-
-local cnt = tonumber(redis.call("incr", "lock:key"))
-if (cnt == 1) then
-    redis.call("pexpire", "lock:key", tonumber("60"))
-elseif (cnt > tonumber(ARGV[1])) then
-    return -1
+-- 获取table的长度
+function table_leng(t)
+  local leng=0
+  for k, v in pairs(t) do
+    leng=leng+1
+  end
+  return leng;
 end
-print(cnt)
-return cnt
+
+---------------------------------------------------------------------------------------------
+local bucket = redis.call('hgetall', 'redis:bucket:limit')
+local num = table_leng(bucket)
+print(num)
+
+redis.call('hset','redis:bucket:limit','lastRefillTime',123)
+
+bucket = redis.call('hgetall', 'redis:bucket:limit')
+local num1 = table_leng(bucket)
+print(num1)
