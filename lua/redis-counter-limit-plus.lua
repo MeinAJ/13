5386a1aa52ae9de418f4c6780ece386a0c6bff21
meinaj_foundao <anjun@foundao.com>
2021-06-09 10:05:27 +0800

    update

--- lua/redis-counter-limit-plus.lua
+++ lua/redis-counter-limit-plus.lua
@@ -3,15 +3,14 @@
 -- count    string  限流数
 -- pexpire  string  单位时间(毫秒)
 
-local key = "redis:bucket:limit:plus"
-local count = "200"
-local pexpire = "1000"
+local key = KEYS[1] -- "redis:bucket:limit:plus"
+local count = ARGV[1] -- "200"
+local pexpire = ARGV[2] --"1000"
 
-local cnt = tonumber(redis.call("incr", key))
-if (cnt == 1) then
-    -- cnt 值为1说明之前不存在该值, 因此需要设置其过期时间
+local remainTokens = tonumber(redis.call("incr", key))
+if (remainTokens == 1) then
     redis.call("pexpire", key, tonumber(pexpire))
-elseif (cnt > tonumber(count)) then
+elseif (remainTokens > tonumber(count)) then
     return -1
 end
-return cnt
+return remainTokens
