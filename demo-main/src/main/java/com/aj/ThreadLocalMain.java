5703d0f1e107e0f4793857443b1f55bd35dfc713
meinaj_foundao <anjun@foundao.com>
2021-06-23 18:50:31 +0800

    update

--- demo-main/src/main/java/com/aj/ThreadLocalMain.java
+++ demo-main/src/main/java/com/aj/ThreadLocalMain.java
@@ -15,34 +15,34 @@ public class ThreadLocalMain {
 
 
     public static final InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
-    public static final InheritableThreadLocal<String> inheritableThreadLocal1 = new InheritableThreadLocal<>();
+//    public static final InheritableThreadLocal<String> inheritableThreadLocal1 = new InheritableThreadLocal<>();
 
     public static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
     private static final ScheduledExecutorService threadPoolExecutor = Executors.newSingleThreadScheduledExecutor();
 
     public static void main(String[] args) {
-//        inheritableThreadLocal.set("Inheritable hello");
+        inheritableThreadLocal.set("Inheritable hello");
 //        inheritableThreadLocal1.set("Inheritable hello 1");
-//        threadLocal.set("hello");
-//        new Thread(() -> {
-//            System.out.println(String.format("子线程可继承值：%s", inheritableThreadLocal.get()));
+        threadLocal.set("hello");
+        new Thread(() -> {
+            System.out.println(String.format("子线程可继承值：%s", inheritableThreadLocal.get()));
 //            System.out.println(String.format("子线程可继承值1：%s", inheritableThreadLocal1.get()));
-//            System.out.println(String.format("子线程值：%s", threadLocal.get()));
-//            new Thread(() -> {
-//                System.out.println(String.format("孙子线程可继承值：%s", inheritableThreadLocal.get()));
+            System.out.println(String.format("子线程值：%s", threadLocal.get()));
+            new Thread(() -> {
+                System.out.println(String.format("孙子线程可继承值：%s", inheritableThreadLocal.get()));
 //                System.out.println(String.format("孙子线程可继承值1：%s", inheritableThreadLocal1.get()));
-//                System.out.println(String.format("孙子线程值：%s", threadLocal.get()));
-//            }).start();
-//
-//        }).start();
+                System.out.println(String.format("孙子线程值：%s", threadLocal.get()));
+            }).start();
 
-        threadPoolExecutor.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + "," + System.currentTimeMillis() / 1000);
-            }
-        }, 0, 1000, TimeUnit.MILLISECONDS);
+        }).start();
+
+//        threadPoolExecutor.scheduleAtFixedRate(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println(Thread.currentThread().getName() + "," + System.currentTimeMillis() / 1000);
+//            }
+//        }, 0, 1000, TimeUnit.MILLISECONDS);
 
         System.out.println("1");
         System.out.println("2");
