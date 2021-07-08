d8ab7d320b7944bc61c0fe15c5b37d20eaf07877
meinaj_foundao <anjun@foundao.com>
2021-07-08 13:49:56 +0800

    update

---
+++ demo-algorithm/src/main/java/com/aj/algorithm/Leetcode46.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019, ABB and/or its affiliates. All rights reserved.
+ * ABB PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.aj.algorithm;
+
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/permutations/
+ *
+ * <p>
+ * 46. 全排列
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * <p>
+ *
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * <p>
+ *
+ * @author An Jun
+ * @date 2021-07-08
+ */
+public class Leetcode46 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+
+}
