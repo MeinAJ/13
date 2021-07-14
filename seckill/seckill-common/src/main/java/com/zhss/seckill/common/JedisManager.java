694a897e88b75f7ec0a56acd730c499172362d3e
unknown <15102356906@163.com>
2021-07-14 22:22:41 +0800

    update

---
+++ seckill/seckill-common/src/main/java/com/zhss/seckill/common/JedisManager.java
@@ -0,0 +1,35 @@
+package com.zhss.seckill.common;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisShardInfo;
+
+import java.util.HashMap;
+
+public class JedisManager {
+
+    private HashMap<String, Jedis> jedisMap = new HashMap<String, Jedis>();
+
+    static class Singleton {
+        static JedisManager instance = new JedisManager();
+    }
+
+    public static JedisManager getInstance() {
+        return Singleton.instance;
+    }
+
+    public Jedis getJedis(String host, int port, String password) {
+        String cacheKey = host + port;
+        if (jedisMap.get(cacheKey) == null) {
+            synchronized (this) {
+                if (jedisMap.get(cacheKey) == null) {
+                    JedisShardInfo shardInfo = new JedisShardInfo(host, port);
+                    shardInfo.setPassword("redis");
+                    Jedis jedis = new Jedis(shardInfo);
+                    jedisMap.put(cacheKey, jedis);
+                }
+            }
+        }
+        return jedisMap.get(cacheKey);
+    }
+
+}
